10e23e7ca101ff8f4df7af4d7603bce7050cc39f
Luigi <l.avila.uvg@gmail.com>
2023-08-11 12:39:46 -0400

    feat: Tabs implemented

---
+++ src/components/Tabs.vue
@@ -0,0 +1,52 @@
+<template>
+    <ion-page>
+        <ion-tabs>
+            <ion-router-outlet></ion-router-outlet>
+            <ion-tab-bar slot="bottom">
+                <ion-tab-button tab="home" href="/home">
+                    <ion-icon :icon="playCircle" />
+                    <ion-label>home</ion-label>
+                </ion-tab-button>
+
+                <ion-tab-button tab="counter" href="/counter">
+                    <ion-icon :icon="radio" />
+                    <ion-label>Counter</ion-label>
+                </ion-tab-button>
+            </ion-tab-bar>
+        </ion-tabs>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import {
+    IonPage,
+    IonTabs,
+    IonRouterOutlet,
+    IonTabBar,
+    IonTabButton,
+    IonLabel,
+    IonIcon,
+} from "@ionic/vue";
+
+import { playCircle, radio, library, search } from "ionicons/icons";
+
+export default {
+    components: {
+        IonPage,
+        IonTabs,
+        IonRouterOutlet,
+        IonTabBar,
+        IonTabButton,
+        IonLabel,
+        IonIcon,
+    },
+    data() {
+        return {
+            playCircle,
+            radio,
+            library,
+            search,
+        };
+    },
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,33 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import Tabs  from "../components/Tabs.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/home',
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
-]
+    path: '/',
+    component: Tabs,
+    children: [
+      {
+        path: '',
+        redirect: '/home',
+      },
+      {
+        path: 'home',
+        component: () => import('../views/HomePage.vue'),
+      },
+      {
+        path: 'counter',
+        component: () => import('../views/CounterPage.vue'),
+      }
+    ],
+  },
+];
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
